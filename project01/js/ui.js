5c156900cd926b37602dae6c03c61e2a4914d219
Mitchell Pomery <bob_george33@hotmail.com>
2014-04-11 12:26:34 +0800

    Add validation icons area

--- project01/js/ui.js
+++ project01/js/ui.js
@@ -2,6 +2,7 @@
 $(document).ready(function() {
 	console.log('Document Loaded');
 	loadShortcuts();
+	validation();
 	//loadLinks();
 });
 
@@ -19,4 +20,12 @@ function loadLinks() {
 	$(".links li").click(function() {
 		console.log(this);
 	});
+}
+
+function validation() {
+	$(
+			'<div class="validation"><a href="http://www.w3.org/html/logo/">' +
+			'<img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered" title="HTML5 Powered">' +
+			'</a></div>'
+	).insertAfter('.logout');
 }
